ab8d02e0b09568c90e2f1af11d0d3b3e36c722d0
gabrieldavidsson-cyber <gabriel.davidsson@student.ksgyf.se>
2026-08-24 07:42:16 +0000

    nytt program

--- Program.cs
+++ Program.cs
@@ -1,4 +1,12 @@
 ﻿// Här börjar du skriva ditt program.
 // Det kan vara smidigt att lösa flera uppgifter i samma program.
 
-Console.WriteLine("Hello, World!");
+Console.Clear();
+Console.WriteLine("Hello, World");
+
+string name = "";
+
+name = Console.ReadLine();
+
+Console.WriteLine("hej "+name);
+Console.Write("nej");
--- obj/Debug/net10.0/programmering1-csharp-template.AssemblyInfo.cs
+++ obj/Debug/net10.0/programmering1-csharp-template.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("programmering1-csharp-template")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a53aa490c2df7945595ca9cda422070bfb33443b")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+64cae433532e1446ea4b946f91ad3adbf864ee05")]
 [assembly: System.Reflection.AssemblyProductAttribute("programmering1-csharp-template")]
 [assembly: System.Reflection.AssemblyTitleAttribute("programmering1-csharp-template")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
